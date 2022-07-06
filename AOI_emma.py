7d7439473a26a149c458e7e864d2727919a79ad6
ICRIN_4 <icrin_4@iMac-de-ICRIN-4.local>
2022-07-06 09:45:02 +0200

    New img with AOI

--- AOI_emma.py
+++ AOI_emma.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-rect1 = patches.Rectangle((355, 40), 45, 45, linewidth=1, edgecolor='r', facecolor='none')
-rect2 = patches.Rectangle((50, 40), 45, 45, linewidth=1, edgecolor='r', facecolor='none')
-rect3 = patches.Rectangle((795, 40), 45, 45, linewidth=1, edgecolor='r', facecolor='none')
-rect4 = patches.Rectangle((795, 40), 45, 45, linewidth=1, edgecolor='r', facecolor='none')
+rect1 = patches.Rectangle((420, 600), 65, 75, linewidth=1, edgecolor='r', facecolor='none')
+rect2 = patches.Rectangle((1585, 600), 65, 75, linewidth=1, edgecolor='r', facecolor='none')
 
 
-number_image = 30
-number_difference = 3
+
+number_image = 20
+number_difference = 2
 
 fig, ax = plt.subplots()
 img = plt.imread(f"../seven_diff/img/img_{number_image}_{number_difference}.png")
@@ -16,7 +15,5 @@ plt.axis('off')
 plt.imshow(img)
 ax.add_patch(rect1)
 ax.add_patch(rect2)
-ax.add_patch(rect3)
-ax.add_patch(rect4)
 
 plt.savefig(f"AOI_seven_diff/img_{number_image}_{number_difference}_aoi.png", bbox_inches="tight", pad_inches=0, dpi=250)
