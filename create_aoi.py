7ea2ad1dca672bd78f6cf55d255770c9dac0b932
mselleki <mselleki98@gmail.com>
2022-07-21 16:42:27 +0200

    pep8 convention

--- create_aoi.py
+++ create_aoi.py
@@ -1,32 +1,30 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-rect1 = patches.Rectangle((120, 180), 200, 200, linewidth=1, edgecolor='r', facecolor='none')
-rect2 = patches.Rectangle((2600, 180), 200, 200, linewidth=1, edgecolor='r', facecolor='none')
-rect3 = patches.Rectangle((1850, 200), 250, 150, linewidth=1, edgecolor='r', facecolor='none')
-rect4 = patches.Rectangle((4330, 200), 250, 150, linewidth=1, edgecolor='r', facecolor='none')
-rect5 = patches.Rectangle((4480, 750), 200, 400, linewidth=1, edgecolor='r', facecolor='none')
-rect6 = patches.Rectangle((2000, 750), 200, 400, linewidth=1, edgecolor='r', facecolor='none')
-rect7 = patches.Rectangle((170, 0), 250, 180, linewidth=1, edgecolor='r', facecolor='none')
-rect8 = patches.Rectangle((2650, 0), 250, 180, linewidth=1, edgecolor='r', facecolor='none')
-rect9 = patches.Rectangle((2050, 180), 150, 350, linewidth=1, edgecolor='r', facecolor='none')
-rect10 = patches.Rectangle((4530, 180), 150, 350, linewidth=1, edgecolor='r', facecolor='none')
-number_image = 2
-number_difference = 5
+"""
+trial 0_0
+rect1 = patches.Rectangle((1927, 693), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+rect2 = patches.Rectangle((1595, 569), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+rect3 = patches.Rectangle((1942, 259), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+rect4 = patches.Rectangle((753, -415), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+"""
+
+# trial 0_2
+rect1 = patches.Rectangle((1468, 624), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+rect2 = patches.Rectangle((803, 1278), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+rect3 = patches.Rectangle((1078, 957), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+rect4 = patches.Rectangle((1519, 628), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
+number_image = 0
+number_difference = 2
 
 fig, ax = plt.subplots()
 img = plt.imread(f"../seven_diff/img/img_{number_image}_{number_difference}.png")
 plt.axis('off')
 plt.imshow(img)
 ax.add_patch(rect1)
-ax.add_patch(rect2)
+# ax.add_patch(rect2)
 ax.add_patch(rect3)
 ax.add_patch(rect4)
-ax.add_patch(rect5)
-ax.add_patch(rect6)
-ax.add_patch(rect7)
-ax.add_patch(rect8)
-ax.add_patch(rect9)
-ax.add_patch(rect10)
-#plt.savefig(f"AOI_seven_diff/img_{number_image}_{number_difference}_aoi.png", bbox_inches="tight", pad_inches=0, dpi=250)
+
+#plt.savefig(f"aoi_seven_diff/img_{number_image}_{number_difference}_aoi.png", bbox_inches="tight", pad_inches=0, dpi=250)
 plt.savefig(f"AOI_seven_diff/test_aoi.png", bbox_inches="tight", pad_inches=0, dpi=250)
